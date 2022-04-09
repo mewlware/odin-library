cc96f1dfaef4b1081b0259328774f67547f8280b
MEL <65815530+mewlware@users.noreply.github.com>
2022-04-09 13:44:41 +0800

    Add delete and add book capability to DOM and object, add edit in DOM only

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Query starting DOM elements
+const libraryWrapper = document.querySelector(".library-wrapper")
+const addBtn = document.querySelector(".add")
+const editBtn = document.querySelector(".edit")
+const stopEditBtn = document.querySelector(".stop-edit")
+
+let books = libraryWrapper.querySelectorAll(":scope > .book");
+
 // Initialize empty library (Object)
 let library = {}
 let indexArray = []
@@ -35,12 +43,154 @@ function uniqueNewBookIndex() {
 
 
 // Adding book to library
-function addBook(index, title, author, read) {
+function addBookObject(index, title, author, read) {
     let newBook = new Book(title, author, read)
     library[index] = newBook;
 }
 
 // Deleting book to library
-function deleteBook(index) {
+function deleteBookObject(index) {
     delete library[index]
 }
+
+// Add book to DOM
+
+function addBookDOM(index, title, author, read) {
+    const newBookDOM = document.createElement("article");
+    newBookDOM.classList.add('book');
+    newBookDOM.dataset.index = index;
+    newBookDOM.dataset.read = read;
+
+    const titleDOM = document.createElement("h3");
+    titleDOM.classList.add('title');
+    titleDOM.textContent = title;
+
+    const authorDOM = document.createElement("p");
+    authorDOM.classList.add('author');
+    authorDOM.textContent = author;
+
+    const readDOM = document.createElement("p");
+    readDOM.classList.add('read');
+    readDOM.textContent = read;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = 'X'
+    deleteBtn.classList.add('delete', 'hidden');
+    deleteBtn.disabled = true;
+
+    newBookDOM.appendChild(titleDOM);
+    newBookDOM.appendChild(authorDOM);
+    newBookDOM.appendChild(readDOM);
+    newBookDOM.appendChild(deleteBtn);
+
+    libraryWrapper.appendChild(newBookDOM);
+}
+
+// Delete book from DOM 
+
+function deleteBookDOM(index) {
+    bookToDelete = document.querySelector(`[data-index="${index}"]`);
+    bookToDelete.remove()
+}
+
+function deleteBtnWrapper(button) {
+    index = button.parentElement.dataset.index;
+    deleteBookDOM(index);
+    deleteBookObject(index);
+}
+
+function enableDeleteBtn(button) {
+    button.disabled = false;
+    button.classList.remove('hidden');
+    button.addEventListener('click', () => {
+        deleteBtnWrapper(button)
+    });
+}
+
+function disableDeleteBtn(button) {
+    button.disabled = true;
+    button.classList.add('hidden');
+    button.removeEventListener('click', deleteBtnWrapper);
+}
+
+function promptAddBook() {
+    let title = prompt("Title?");
+    let author = prompt("Author?");
+    let read = confirm("Read or Not? Ok = read, cancel = not");
+    let index = uniqueNewBookIndex();
+
+    addBookObject(index, title, author, read);
+    addBookDOM(index, title, author, read);
+    console.log(library)
+}
+
+function disabledAddBtn() {
+    addBtn.disabled = true;
+}
+
+function enableAddBtn() {
+    addBtn.disabled = false;
+}
+
+function toggleEditBtns() {
+    editBtn.classList.toggle('hidden');
+    stopEditBtn.classList.toggle('hidden');
+
+    if (editBtn.classList.contains('hidden')) {
+        editBtn.disabled = true;
+        stopEditBtn.disabled = false;
+    } else if (stopEditBtn.classList.contains('hidden')) {
+        stopEditBtn.disabled = true;
+        editBtn.disabled = false;
+    }
+}
+
+function enableEditing() {
+    books.forEach((book) => { 
+        for (i = 0; i < book.children.length; i++) {
+            if (book.children[i].classList.contains('title')) {
+                book.children[i].contentEditable = "true";
+            } else if (book.children[i].classList.contains('author')) {
+                book.children[i].contentEditable = "true";
+            } else if (book.children[i].classList.contains('delete')) {
+                book.children[i].disabled = false;
+                book.children[i].classList.toggle('hidden')
+                enableDeleteBtn(book.children[i])
+            }
+        }
+    })
+}
+
+function disableEditing() {
+    books.forEach((book) => {
+        for (i = 0; i < book.children.length; i++) {
+            if (book.children[i].classList.contains('title')) {
+                book.children[i].contentEditable = "false";
+            } else if (book.children[i].classList.contains('author')) {
+                book.children[i].contentEditable = "false";
+            } else if (book.children[i].classList.contains('delete')) {
+                disableDeleteBtn(book.children[i])
+            }
+        }
+    })
+}
+
+function editMode() {
+    books = libraryWrapper.querySelectorAll(":scope > .book");
+    enableEditing();
+    toggleEditBtns();
+    disabledAddBtn();
+}
+
+function viewMode() {
+    disableEditing();
+    toggleEditBtns();
+    enableAddBtn();
+}
+
+// Starting 
+
+// Event Listeners
+editBtn.addEventListener('click', editMode)
+addBtn.addEventListener('click', promptAddBook)
+stopEditBtn.addEventListener('click', viewMode)
